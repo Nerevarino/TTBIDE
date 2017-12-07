3c7f69b33bb350bd6a3ee2b38dcb996b851c54b5
Evgeniy Dronov <dronov-ep@yandex.ru>
2017-12-07 15:57:06 +0300

    Checked wl_interfaces

--- application.cpp
+++ application.cpp
@@ -11,7 +11,6 @@ Application::~Application()
 
 }
 
-
 int Application::run()
 {
     display = wl_display_connect(nullptr);
@@ -26,13 +25,13 @@ int Application::run()
     }
 
     wl_registry_add_listener(registry, &registery_listener, this);
-    wl_display_dispatch(display);
+    int success_count = wl_display_dispatch(display);
 
 
+    cout << success_count << " events has been processed" << endl;
     cout << "Work finished!" << endl;
     return 0;
 }
-
 
 void Application::onAddWaylandObject
     (
@@ -44,6 +43,8 @@ void Application::onAddWaylandObject
     )
 {
     Application* app = (Application*)client_data;
+
+    cout << "Here is a registry event for " << interface_name << " with id = " << object_id << endl;
 }
 
 void Application::onRemoveWaylandObject
